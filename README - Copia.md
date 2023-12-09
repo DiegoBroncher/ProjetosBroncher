1f30f31e930d02669d9e3c3ecdb185cd76a53cf4
Diego Broncher <diegobroncher@gmail.com>
2023-12-09 14:55:47 +0200

    atualizacoes de js

---
+++ README - Copia.md
@@ -0,0 +1,8 @@
+Um arquivo ReadMe (Leia-me) serve para orientar quem for ler sobre o projeto em questao
+
+No caso, os arquivos HTML sao de estudos da linguagem de estruturacao HTML(HyperText Markup Language) 
+e arquivos .css (Cascating Styling Sheets)
+
+Em estudos futuros haverao arquivos .js(Javascript)
+
+Por hora existem arquivos de ensaios sobre o uso de HTML e CSS, simples diagramacao e estilizacao sem uso de tableless ou paginas responsivas
--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 let texto = "apos fechar este alerta, pressione F12e clique na aba Console ";
 let texto2 = "este eh um simples texto";
-
+/*
 alert(texto);
 console.log(texto2);
-
+*/
 // esta eh uma linha de comentario
 /**
  * esta eh uma segunda forma de fazer linhas de comentarios
@@ -25,12 +25,12 @@ function minhaFuncao(parametro1, parametro2, parametro3){//parametros sao valore
 function somar(valor1, valor2){
     console.log(valor1 + valor2);
 }
-
+/*
 //usamos a funcao assim
 somar(1,1);
 somar(2,10);
 somar(-1,-1);
-
+*/
 
 
 
@@ -42,9 +42,61 @@ for(let i = 0; i < meuArray.length; i++){ // valor inical eh zero, se for menor 
     
     // meuArray tem varios valores, portanto dizer que vai acessar o valor na posicao "i", que 
     // comeca com o numero zero, nao comeca no numero um, todo array comeca com o index 0
-    console.log(meuArray[i])
-    // neste caso, este array tem 4 valores, portanto ele percorre 0, 1, 2 e 3, nao tem o 
+    //// console.log(meuArray[i])
+    // este caso, este array tem 4 valores, portanto ele percorre 0, 1, 2 e 3, nao tem o 
     // indice 4 pois seria o 5 elemento do array que nao caso nao existe
 
 }
+let meuConteudo = document.getElementById('conteudo');
 
+for(let i = 0 ; i < 10 ; i++){
+    if(i==0){
+        meuConteudo.innerHTML += ('<input type="button" value="mova-me" onClick="mover()" /><br />');
+    }
+    else{
+        meuConteudo.innerHTML += (':'+ i + '<br />');
+    }
+    //console.log(i, i + 1);
+}
+
+
+function mover(){
+    let minhaAnimacao = document.getElementById('animacao');
+    minhaAnimacao.style.position = 'absolute';
+    console.log(minhaAnimacao.style)
+    minhaAnimacao.style.left = 10+'px';
+    let meusPIxels = 10;
+    setInterval(()=>{
+        if(meusPIxels < 1000){
+            console.log(minhaAnimacao.style.left)
+            minhaAnimacao.style.left = (meusPIxels+'px');
+            meusPIxels+=10;
+        }
+    },100)
+}
+
+
+
+//JSON: JavaScript Object Notation
+let diego = {
+    nome: 'Diego',
+    telefone: '123456789',
+    idade: 44
+}
+let gilbert = {
+    nome: 'Gilbert',
+    telefone: '124578963',
+    idade: 46
+}
+let cadastro = [diego, gilbert]
+
+
+cadastro.forEach(pessoa => {
+    console.log(pessoa)
+    meuConteudo.innerHTML+="<ul><li>"+pessoa.nome+"</li><li>"+pessoa.telefone+"</li><li>"+pessoa.idade+"</li></ul>";   
+})
+
+//console.log(cadastro)
+fetch('https://jsonplaceholder.typicode.com/todos/2')
+      .then(response => response.json())
+      .then(json => console.log(json))
